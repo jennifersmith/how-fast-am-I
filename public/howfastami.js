2c869beeba1ac5bc69b18849917efb3cc91a71ce
jennifersmith <jennifer.e.m.smith@gmail.com>
2010-06-20 12:24:43 +0100

    made jsonp controller

--- public/howfastami.js
+++ public/howfastami.js
@@ -20,14 +20,14 @@ var howFastAmI = {
 						},
 						{
 							name: "escapes",
-							url: "home/getthings?dataset=escapes"
+							url: "http://how-fast-am-i.heroku.com/home/getthings?dataset=escapes"
 						}
 						]
 					,
 			distance: [
 							{
 							    name: "trainlines",
-							    url: "home/getthings?dataset=lines"
+							    url: "http://how-fast-am-i.heroku.com/home/getthings?dataset=lines"
 							}
 					   ]
 		} ,
