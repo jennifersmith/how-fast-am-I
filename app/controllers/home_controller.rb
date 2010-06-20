530538101fd75bb2747273955f24a7ac34154afc
jennifersmith <jennifer.e.m.smith@gmail.com>
2010-06-20 08:14:11 +0100

    Added support for sucking in dbperdia data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,27 @@
+require 'rest_client'
+
 class HomeController < ApplicationController
+	def initialize
+		@queries = {:motorways => 
+"SELECT *
+
+  WHERE
+
+    { 
+
+      ?motorway  a   <http://dbpedia.org/ontology/Road>     .
+
+      ?motorway <http://xmlns.com/foaf/0.1/name>    ?name  .
+
+      ?motorway <http://dbpedia.org/ontology/length>    ?length
+
+    }"}
+	end
+	def getthings 
+		queryEscaped = CGI::escape(@queries[params[:dataset].intern]||@queries.first[1])
+		puts  'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&should-sponge=&query=' + queryEscaped + '&format=text%2Fhtml&debug=on&timeout='
+		res = RestClient.get 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&should-sponge=&query=' + queryEscaped + '&format=text%2Fhtml&debug=on&timeout='
+				
+		render :text=>res
+	end
 end
