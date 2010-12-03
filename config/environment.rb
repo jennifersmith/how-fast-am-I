2ea1abb43b5d75ac89a6f49260a5fb3fd1d7ffe7
Jen Smith <jennifer.e.m.smith@gmail.com>
2010-12-03 22:35:20 +0000

    Added cucumber rspec and first test + haml support

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -34,6 +33,7 @@ Rails::Initializer.run do |config|
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
+  config.gem "haml"
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
